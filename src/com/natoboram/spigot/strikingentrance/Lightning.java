6fb31ca9a0c10bfdd99a5b649eb26677a1f4a098
Nato Boram <nato.boram@gmail.com>
2020-05-19 17:07:50 -0400

    final input variables

--- src/com/natoboram/spigot/strikingentrance/Lightning.java
+++ src/com/natoboram/spigot/strikingentrance/Lightning.java
@@ -9,17 +9,17 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class Lightning implements Listener {
 
 	@EventHandler
-	public void onPlayerDeath(PlayerDeathEvent event) {
+	public void onPlayerDeath(final PlayerDeathEvent event) {
 		event.getEntity().getWorld().strikeLightningEffect(event.getEntity().getLocation());
 	}
 
 	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent event) {
+	public void onPlayerJoin(final PlayerJoinEvent event) {
 		event.getPlayer().getWorld().strikeLightningEffect(event.getPlayer().getLocation());
 	}
 
 	@EventHandler
-	public void onPlayerQuit(PlayerQuitEvent event) {
+	public void onPlayerQuit(final PlayerQuitEvent event) {
 		event.getPlayer().getWorld().strikeLightningEffect(event.getPlayer().getLocation());
 	}
 }
--- src/com/natoboram/spigot/strikingentrance/StrikingEntrance.java
+++ src/com/natoboram/spigot/strikingentrance/StrikingEntrance.java
@@ -3,8 +3,10 @@ package com.natoboram.spigot.strikingentrance;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StrikingEntrance extends JavaPlugin {
+
 	@Override
 	public void onEnable() {
+
 		// Enable Listeners
 		getServer().getPluginManager().registerEvents(new Lightning(), this);
 	}
